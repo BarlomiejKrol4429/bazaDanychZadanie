daed6f5a84cd856b0928dbb4eb90e6085a7e8cc6
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2026-03-06 10:23:08 +0100

    Klasa Ksiażki

--- app/src/main/java/com/example/bazazadanie/Ksiazki.java
+++ app/src/main/java/com/example/bazazadanie/Ksiazki.java
@@ -1,6 +1,88 @@
 package com.example.bazazadanie;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "ksiazki_tabela")
 public class Ksiazki {
-    private int ilosc_stron;
-    private  autor
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String autor;
+    private String tytul;
+    private double cena;
+    @ColumnInfo(name = "ilosc_stron")
+    private int iloscStron;
+    @ColumnInfo(name = "rok_wydania")
+    private int rokWydania;
+
+    public Ksiazki(int id, String autor, String tytul, double cena, int iloscStron, int rokWydania) {
+        this.id = 0;
+        this.autor = autor;
+        this.tytul = tytul;
+        this.cena = cena;
+        this.iloscStron = iloscStron;
+        this.rokWydania = rokWydania;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public void setTytul(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public int getIloscStron() {
+        return iloscStron;
+    }
+
+    public void setIloscStron(int iloscStron) {
+        this.iloscStron = iloscStron;
+    }
+
+    public int getRokWydania() {
+        return rokWydania;
+    }
+
+    public void setRokWydania(int rokWydania) {
+        this.rokWydania = rokWydania;
+    }
+
+    @Override
+    public String toString() {
+        return "Ksiazki{" +
+                "id=" + id +
+                ", autor='" + autor + '\'' +
+                ", tytul='" + tytul + '\'' +
+                ", cena=" + cena +
+                ", iloscStron=" + iloscStron +
+                ", rokWydania=" + rokWydania +
+                '}';
+    }
 }
+
---
+++ app/src/main/java/com/example/bazazadanie/KsiazkiDao.java
@@ -0,0 +1,8 @@
+package com.example.bazazadanie;
+
+import androidx.room.Dao;
+
+@Dao
+public interface KsiazkiDao {
+
+}
