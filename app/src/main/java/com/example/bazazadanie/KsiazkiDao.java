8bd05ab9c00d66d47c8f392b96d900bcb697c3c4
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2026-03-06 10:56:51 +0100

    Zdefiniowanie Dao i klasy abstrakcyjnej bazy

--- app/src/main/java/com/example/bazazadanie/KsiazkiDao.java
+++ app/src/main/java/com/example/bazazadanie/KsiazkiDao.java
@@ -1,8 +1,23 @@
 package com.example.bazazadanie;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface KsiazkiDao {
-
+    @Insert
+    public void wstawKsiazkeDoBazy(Ksiazki ksiazka);
+    @Insert
+    public void wstawKilkaKsiazek(Ksiazki ...ksiazka);
+    @Delete
+    public void usunZBazy(Ksiazki ksiazka);
+    @Update
+    public void zaktualizuj(Ksiazki ksiazka);
+    @Query("SELECT * FROM ksiazki_tabela")
+    List<Ksiazki> zwrocWszytkieWypiekiZBazy();
 }
---
+++ app/src/main/java/com/example/bazazadanie/KsiazkiDatabase.java
@@ -0,0 +1,23 @@
+package com.example.bazazadanie;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Ksiazki.class}, version = 1)
+public abstract class KsiazkiDatabase extends RoomDatabase{
+    public abstract Ksiazki zwrocKsiazkiDao();
+
+    private static KsiazkiDatabase instancja;
+
+    public static KsiazkiDatabase zwrocInstancjeBazyDanych(Context context){
+        if(instancja == null){
+            instancja = Room.databaseBuilder(
+                    context, KsiazkiDatabase.class, "przepisy_db"
+            ).allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        }
+        return instancja;
+    }
+}
